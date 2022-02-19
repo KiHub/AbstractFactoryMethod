592dfc655e38dc3aabe55729bff4a9630c7202aa
Kirill Ki <kirillkalapov@gmail.com>
2022-02-19 20:05:20 +0100

    Initial Commit

---
+++ AbstractFactoryMethod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AbstractFactoryMethod
+//
+//  Created by  Mr.Ki on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
